56b6c0bff9d138ce9c0c190b90c16faceb0df51c
student <student@10.2.3.9>
2016-05-23 14:56:46 +0200

    Test parametryczny

--- pl.edu.us.awpto/src/test/java/kolokwium/a/pl/edu/us/awpto/FizzBuzzTest.java
+++ pl.edu.us.awpto/src/test/java/kolokwium/a/pl/edu/us/awpto/FizzBuzzTest.java
@@ -1,9 +1,41 @@
 package kolokwium.a.pl.edu.us.awpto;
 
 import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collection;
+ 
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+@RunWith(Parameterized.class)
 public class FizzBuzzTest
 {
+	@Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {     
+                 { 1,  "1"}, { 3, "Fizz" }, { 5, "Buzz" }, { 15, "FizzBuzz" }
+           });
+    }
+
+    private int fInput;
+
+    private String fExpected;
+
+    public FizzBuzzTest(int input, String expected) {
+        fInput= input;
+        fExpected= expected;
+    }
+    
+	
+	@Test
+    public void test() {
+		FizzBuzz tester = new FizzBuzz();
+        assertEquals("",fExpected, tester.calculate(fInput));
+    }
 }
